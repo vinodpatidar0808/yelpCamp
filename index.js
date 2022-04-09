f9651049d7a226432b408130d9bfe1282722060a
vinod_0808 <vinodpatidar0808@gmail.com>
2022-04-09 19:09:07 +0530

    minor changes to index.js

--- index.js
+++ index.js
@@ -22,8 +22,7 @@ const helmet = require("helmet");
 const MongoStore = require("connect-mongo");
 const port = process.env.PORT || 3000;
 
-// const dbUrl = process.env.DB_URL;
-const dbUrl = "mongodb://localhost:27017/yelp-camp";
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
